fbb4dea9f59f9e1c753cd02fbcaeb41b34261cf5
Douglas <149517402+DougNSantos@users.noreply.github.com>
2024-12-15 16:31:21 -0300

    Add files via upload

---
+++ VetorAlfabeto/VetorAlfabeto/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorAlfabeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] alfabeto = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l',
+                                      'm','n','o','p','q','r','s','t','u','v','x','y','z'};            
+
+            string texto;
+
+            Console.WriteLine("DIGITE SUA PALAVRA:");
+            texto = Console.ReadLine();
+
+            char[] palavra = texto.ToCharArray();
+
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                int conversaoTexto = 0;
+                
+                for(int j = 0; i < alfabeto.Length; j++)
+                {
+                    if (alfabeto[j].Equals(palavra[i]))
+                    {
+                        conversaoTexto = j + 1;
+                        break;
+                    }
+                }
+
+                Console.Write(" " + conversaoTexto + " ");
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
